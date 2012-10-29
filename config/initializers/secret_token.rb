1fca43ce32758e85e28b79809efc6d317106dc23
segfault <s9gf4ult@gmail.com>
2012-10-29 12:40:02 +0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ruvote::Application.config.secret_token = '29d34801ce6fbcd17d3e37c1a029f61765c3ffa419a8ac1c5279183dd96bd41d68bfcf38a95255217aa85a6f243e784fae77d53c4c1ad146a0a49ccee19e7daa'
