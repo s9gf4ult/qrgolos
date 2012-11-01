c792d1bc0cb118f8769d9cb90c497724d30b163b
segfault <s9gf4ult@gmail.com>
2012-11-01 18:00:14 +0600

    * Question model fixed

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,5 +1,6 @@
 class Section < ActiveRecord::Base
   attr_accessible :name
+  has_many :questions
   belongs_to :meeting
   validates :name, :presence => true
   validates :name, :uniqueness => {:scope => :meeting_id}
