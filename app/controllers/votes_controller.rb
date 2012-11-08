37fdf765f49d823e55fd87b01ab6d18c11f9685d
segfault <s9gf4ult@gmail.com>
2012-11-08 16:04:45 +0600

    middle

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,18 +1,26 @@
 class VotesController < ApplicationController
 
   def radio_vote
-    answer_variant = AnswerVariant.find params[:answer_variant][:id]
-    anonymous = Anonymous.find params[:id]
-    vote = anonymous.votes.build {:answer_variant => answer_variant, :vote => 1}
-    if vote.save
-      respond_to do |format|
+    params[:id] = params[:_id]
+    begin
+      answer_variant = AnswerVariant.find params[:answer_variant][:id]
+    rescue 
+      flash[:notice] = t 'votes.choose_variant'
+      redirect_to 
+    end
+    anonymous = Anonymous.find params[:_id]
+    vote = anonymous.votes.build :answer_variant => answer_variant, :vote => 1
+    respond_to do |format|
+      if vote.save
         format.html do
           flash[:notice] = t 'votes.thanks'
-          redirect_to radio_path(anonymous.aid)
+          render :action => 's#show'
         end
-        format.json {
-        }                       #  FIXME: complete
-
-
+        format.json { head :no_content, :status => :created }
+      else
+        format.html { render :action => 's#show' }
+        format.json { render :json => vote.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 end
