3d3a0a3fcc0c355518f1a8c3e71ac6c482ecae65
segfault <s9gf4ult@gmail.com>
2012-11-15 14:53:21 +0600

    + devise forms added

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,21 @@
+class LoginController < ApplicationController
+  layout :get_layout
+  
+  def registration
+    
+  end
+
+  def forgot_password
+
+  end
+
+  def login
+
+  end
+
+  private
+
+  def get_layout
+    'empty'
+  end
+end
