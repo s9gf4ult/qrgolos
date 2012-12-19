0f3bcdc688e51a8702452a97c97257080266dee8
segfault <s9gf4ult@gmail.com>
2012-12-19 11:45:46 +0600

    mobile javascript done

--- app/assets/javascripts/mobile/mobile.js
+++ app/assets/javascripts/mobile/mobile.js
@@ -1,7 +1,11 @@
 // all dialog buttons should close their parent dialog
-$(".ui-dialog button").live("click", function() {
-	$("[data-role='dialog']").dialog("close");
-});
 $(document).on("mobileinit", function(){
 	$.mobile.defaultPageTransition = "slide";
+    $(".ui-dialog button").live("click", function() {
+	    $("[data-role='dialog']").dialog("close");
+    });
+
+});
+$(function(){
+    $.mobile.ajaxEnabled = false;
 });
